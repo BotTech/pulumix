b3e8ef89f2eec950d68e555f58e1adb0901ed046
Jason Pickens <jasonpickensnz@gmail.com>
2023-06-04 19:46:41 +1200

    Add createBucket policy

--- packages/pulumix-aws/iam/policies/documents/statements/s3.ts
+++ packages/pulumix-aws/iam/policies/documents/statements/s3.ts
@@ -1,6 +1,8 @@
 import * as aws from "@pulumi/aws";
 import * as statements from "./statements";
 import { AccessPatternArgs, allowResourceActions } from "./statements";
+import { Output } from "@pulumi/pulumi";
+import * as pulumi from "@pulumi/pulumi";
 
 type BucketArgs = Pick<aws.s3.BucketV2, "bucket">;
 type BucketPrefixArgs = Pick<aws.s3.BucketV2, "bucketPrefix">;
@@ -8,17 +10,22 @@ type BucketPrefixArgs = Pick<aws.s3.BucketV2, "bucketPrefix">;
 export type CreateBucketArgs = (BucketArgs | BucketPrefixArgs) &
   AccessPatternArgs;
 
+export function bucketName(args: CreateBucketArgs): Output<string> {
+  return "bucket" in args
+    ? args.bucket
+    : pulumi.interpolate`${args.bucketPrefix}*`;
+}
+
 class S3AccessPatterns extends statements.AccessPatterns {
   constructor() {
     super("S3", "s3");
   }
 
   createBucket(args: CreateBucketArgs): aws.iam.PolicyStatement {
-    const resource = "bucket" in args ? args.bucket : `${args.bucketPrefix}*`;
     return allowResourceActions({
       Sid: "CreateBucketAccess",
       Action: ["s3:CreateBucket", "s3:PutBucketTagging"],
-      Resource: resource,
+      Resource: bucketName(args),
       mfaPresent: args.mfaPresent,
     });
   }
--- packages/pulumix-aws/iam/policies/s3.ts
+++ packages/pulumix-aws/iam/policies/s3.ts
@@ -1,6 +1,10 @@
 import * as aws from "@pulumi/aws";
 import * as pulumi from "@pulumi/pulumi";
 import * as documents from "./documents";
+import {
+  bucketName,
+  CreateBucketArgs,
+} from "@src/iam/policies/documents/statements/s3";
 
 export function fullAccess(
   opts?: pulumi.CustomResourceOptions
@@ -10,6 +14,23 @@ export function fullAccess(
     {
       description: "Allows full access to S3.",
       policy: documents.s3.fullAccess(),
+    },
+    opts
+  );
+}
+
+export function createBucket(
+  name: string,
+  args: CreateBucketArgs,
+  opts?: pulumi.CustomResourceOptions
+): aws.iam.Policy {
+  return new aws.iam.Policy(
+    name,
+    {
+      description: pulumi.interpolate`Allows access to create the bucket ${bucketName(
+        args
+      )}`,
+      policy: documents.s3.createBucket(args),
     },
     opts
   );
