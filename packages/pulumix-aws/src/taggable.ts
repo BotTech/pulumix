b26db4e04bab3da3a3c2eb6e28dca372156ab4fd
Jason Pickens <jasonpickensnz@gmail.com>
2024-05-13 21:08:54 +1200

    Check for taggable resource validation

--- packages/pulumix-aws/src/taggable.ts
+++ packages/pulumix-aws/src/taggable.ts
@@ -1,20 +1,48 @@
 import * as pulumi from "@pulumi/pulumi";
+import * as policy from "@pulumi/policy";
 
 import { TAGGABLE_TYPES } from "./taggable-types";
 export * from "./taggable-types";
 
-export type Tags = pulumi.Input<{
-  [key: string]: pulumi.Input<string>;
-}>;
+export type Tags = Record<string, string>;
 
-export type TaggableProps = {
+export type TaggableProps = Omit<
+  Pick<policy.ResourceValidationArgs, "props">,
+  "tags"
+> & {
   tags?: Tags;
 };
 
-export type TaggableResourceTransformationArgs =
-  pulumi.ResourceTransformationArgs & {
-    props: TaggableProps;
-  };
+export type TaggableResourceValidationArgs = Omit<
+  policy.ResourceValidationArgs,
+  "props"
+> & {
+  props: TaggableProps;
+};
+
+export function isTaggableResourceValidation(
+  args: policy.ResourceValidationArgs,
+): args is TaggableResourceValidationArgs {
+  return TAGGABLE_TYPES.includes(args.type);
+}
+
+export type TagsInput = pulumi.Input<{
+  [key: string]: pulumi.Input<string>;
+}>;
+
+export type TaggableInputProps = Omit<
+  Pick<pulumi.ResourceTransformationArgs, "props">,
+  "tags"
+> & {
+  tags?: TagsInput;
+};
+
+export type TaggableResourceTransformationArgs = Omit<
+  pulumi.ResourceTransformationArgs,
+  "props"
+> & {
+  props: TaggableInputProps;
+};
 
 export function isTaggableResourceTransformation(
   args: pulumi.ResourceTransformationArgs,
@@ -23,9 +51,9 @@ export function isTaggableResourceTransformation(
 }
 
 export function appendTagsToProps(
-  props: TaggableProps,
-  tags: Tags,
-): TaggableProps {
+  props: TaggableInputProps,
+  tags: TagsInput,
+): TaggableInputProps {
   return {
     ...props,
     tags: pulumi.all([props.tags, tags]).apply(([existingTags, tags]) => {
@@ -34,7 +62,7 @@ export function appendTagsToProps(
   };
 }
 
-export function registerAppendTagsTransformation(tags: Tags): void {
+export function registerAppendTagsTransformation(tags: TagsInput): void {
   pulumi.runtime.registerStackTransformation((args) => {
     if (isTaggableResourceTransformation(args)) {
       return { props: appendTagsToProps(args.props, tags), opts: args.opts };
